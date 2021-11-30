64ca74774efd98247ec3c963d0e44703ac37892f
nsasso <n.sasso@it-students.fr>
2021-11-30 17:05:01 +0100

    Update. ajout date/heure et taux humidite

--- water_modif.py
+++ water_modif.py
@@ -7,6 +7,8 @@ import time
 init = False
 bus = SMBus(1)
 ads7830_commands = (0x84, 0xc4, 0x94, 0xd4, 0xa4, 0xe4, 0xb4, 0xf4)
+maxHumidity = 220
+maxWet = 100
 
 GPIO.setmode(GPIO.BOARD) # Broadcom pin-numbering scheme
 
@@ -19,17 +21,13 @@ def get_last_watered():
 
 def read_ads7830(input):
     bus.write_byte(0x4b, ads7830_commands[input])
-    return bus.read_byte(0x4b)
-      
+    return bus.read_byte(0x4b)    
+
 def get_status(input = 0):
     value = read_ads7830(input)
-    print(value)
-    if (value > 150):
-        print("Sec")
-        return 0
-    else:
-        print("Mouillé")
-        return 1
+    value = int((value - maxHumidity) * (100 - 0) / (maxWet - maxHumidity) + 0)
+    print(str(value) + "%")
+    return value
     
 def auto_water(delay = 5, pump_pin = 40, water_sensor_input = 0):
     consecutive_water_count = 0
@@ -37,7 +35,7 @@ def auto_water(delay = 5, pump_pin = 40, water_sensor_input = 0):
     try:
         while 1 and consecutive_water_count < 10:
             time.sleep(delay)
-            wet = get_status(input = water_sensor_input)
+            wet = (get_status(0) > 20)
             print(consecutive_water_count)
             if not wet:
                 if consecutive_water_count < 5:
@@ -48,7 +46,7 @@ def auto_water(delay = 5, pump_pin = 40, water_sensor_input = 0):
     except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
         GPIO.cleanup() # cleanup all GPI
 
-def pump_on(pump_pin = 40, delay = 3):
+def pump_on(pump_pin = 40, delay = 1):
     f = open("last_watered.txt", "w")
     f.write("Dernier arrosage le {}".format(datetime.datetime.now().strftime("%x à %X")))
     f.close()
--- web_plants.py
+++ web_plants.py
@@ -5,63 +5,63 @@ import RPi.GPIO as GPIO
 import water_modif as water
 import os
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
-def template(title = "Happy plant :)", text = ""):
-    dateNow = datetime.datetime.now().strftime("%x")
-    timeNow = datetime.datetime.now().strftime("%H:%M")
+def template(title = "Happy plant :)", text = "", humid = 0):
+    dateNow = datetime.datetime.now().strftime("%d/%m/%Y")
     templateDate = {
         'title' : title,
         'date' : dateNow,
-        'time' : timeNow,
-        'text' : text
+        'text' : text,
+        'humid' : humid 
         }
     return templateDate
 
 @app.route("/")
-def hello():
-    templateData = template()
+def home():
+    templateData = template(humid = water.get_status())
+    
     return render_template('main.html', **templateData)
 
 @app.route("/last_watered")
 def check_last_watered():
-    templateData = template(text = water.get_last_watered())
+    templateData = template(text = water.get_last_watered(), humid = water.get_status())
     return render_template('main.html', **templateData)
 
 @app.route("/sensor")
 def action():
     status = water.get_status()
     message = ""
-    if (status == 0):
+    if (status < 20):
         message = "J'ai soif :("
     else:
-        message = "J'me sens bien :)"
+        message = "Je me sens bien :)"
 
-    templateData = template(text = message)
+    templateData = template(text = message, humid = water.get_status())
     return render_template('main.html', **templateData)
 
 @app.route("/water")
 def action2():
     water.pump_on()
-    templateData = template(text = "Arrosée")
+    templateData = template(text = "Arrosée", humid = water.get_status())
     return render_template('main.html', **templateData)
 
 @app.route("/auto/water/<toggle>")
 def auto_water(toggle):
     running = False
     if toggle == "ON":
-        templateData = template(text = "Arrosage automatique activé")
+        templateData = template(text = "Arrosage automatique activé", humid = water.get_status())
         for process in psutil.process_iter():
             try:
                 if process.cmdline()[1] == 'auto_water.py':
-                    templateData = template(text = "Arrosage déjà activé")
+                    templateData = template(text = "Arrosage déjà activé", humid = water.get_status())
                     running = True
             except:
                 pass
         if not running:
             os.system("python3.9 auto_water.py&")
     else:
-        templateData = template(text = "Arrosage desactivé")
+        templateData = template(text = "Arrosage desactivé", humid = water.get_status())
         os.system("pkill -f auto_water.py")
 
     return render_template('main.html', **templateData)
